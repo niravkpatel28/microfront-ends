ac3607e4ed86402d8a93a669d4e4546b3de9cbbb
niravkpatel28 <niravkpatel28@gmail.com>
2022-06-06 17:46:48 +0530

    Container build #12

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MaketingApp";
 export default () => {
   return (
     <div>
-      <h1>Root Application:#11</h1>
+      <h1>Root Application:#12</h1>
       <hr />
       <MarketingApp />
     </div>
