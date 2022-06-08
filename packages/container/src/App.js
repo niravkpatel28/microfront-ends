34e0df40684d138995f753d1c29ee6881c263eda
niravkpatel28 <niravkpatel28@gmail.com>
2022-06-08 16:26:08 +0530

    Adding generateClass and Style Provide to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 // import { mount } from "marketingApp/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MaketingApp";
 import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName();
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        {/* <h1>Root Application:#12</h1> */}
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          {/* <h1>Root Application:#12</h1> */}
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
